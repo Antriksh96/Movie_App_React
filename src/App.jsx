8df59ce1ed4f7e03812814e44a74478154780f49
Antriksh Lakde <antrikshlakde@gmail.com>
2025-11-14 21:08:43 +0530

    Main page half work done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
-import { useState } from 'react'
+import { meta } from '@eslint/js'
+import { use } from 'react';
+import { useEffect, useState } from 'react'
 
 function App() {
 
-   async function getData() {
-    const response = await fetch('')
-    const data = await response.json()
-   }
+  const [searchterm, setSearchterm] = useState('')
+  const [data, setData] = useState(null);
+
+  const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+  const API_OPTION = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${API_KEY}`
+    }
+  }
+
+  useEffect(() => {
+
+    async function getTrendingMovies() {
+      try {
+        // const response = await fetch(`https://api.themoviedb.org/3/trending/movie/day?language=en-US`, API_OPTION)
+        const jsondata = await response.json();
+        console.log(jsondata)
+        setData(jsondata)
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getTrendingMovies();
+  }, [])
+
+
+  function fetchenter(e) {         //As user press enter button after typing any movie name in search input the fun call the fun that fetch data from api.
+    if (e.key == 'Enter') {
+      // getData();
+    }
+    console.log(e.key)
+  }
+
+
 
   return (
     <main>
@@ -19,12 +55,18 @@ function App() {
           <div className='search'>
             <div>
               <img src="search.png" alt="" />
-              <input type="text" placeholder='Search through 300+ movies online' />
+              <input value={searchterm} type="text" placeholder='Search through 300+ movies online' onKeyDown={(e) => { fetchenter(e) }} onChange={(e) => { setSearchterm(e.target.value) }} />
             </div>
           </div>
           <div className='trending'>
-            <h2>Trending</h2>
-
+            <div><span className='text-white absolute text-4xl translate-y-[-60px]'>Trending</span></div>
+            <div>
+              <ul>  
+              {
+                data ? data.results ? data.results.map((e, index) => <li key={e.id}> <p>{index + 1}</p> <img src={`https://image.tmdb.org/t/p/w500${e.backdrop_path}`} /> </li>) : null : null
+              }
+              </ul>
+            </div>
           </div>
         </div>
       </div>
---
+++ src/components/Moviecard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Moviecard() {
+  return (
+    <div className='movie-card'>
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default Moviecard
